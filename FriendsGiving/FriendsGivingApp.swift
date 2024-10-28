786cf659ed702bb1560b89ec2baba5f1613cdfc6
gallaugher <john.gallaugher@bc.edu>
2024-10-28 17:47:07 -0400

    Initial Commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,14 @@
+// File: FriendsGivingApp.swift Project: FriendsGiving
+// Created by: Prof. John Gallaugher on 10/28/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
